394953ff0cf17af07be77474610c7fe158bc8aba
Francesco Romani <fromani@redhat.com>
2020-12-01 20:05:16 +0100

    runner: propagate the exit code of inner script
    The inner script (custom/...) is the real test
    wrapper, so the runner code should actually return
    that return code, and not swallow it.
    
    This is especially important to propagate test failures.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- runner/nfd.sh
+++ runner/nfd.sh
@@ -7,5 +7,7 @@ BASEDIR=$( dirname $(readlink -f ${BASH_SOURCE[0]} ) )
 export KUBECONFIG=$( ${BASEDIR}/../cluster/kind-replica1.sh start nfd-e2e )
 ${BASEDIR}/../environment/topology-aware-scheduling-quick.sh start
 ${BASEDIR}/../custom/nfd.sh nfd-e2e ${KUBECONFIG}
+RET="$?"
 ${BASEDIR}/../environment/topology-aware-scheduling-quick.sh stop
 ${BASEDIR}/../cluster/kind-replica1.sh stop nfd-e2e
+exit ${RET}
