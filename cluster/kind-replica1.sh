3b17a0bfecba29bf31c5800cbd5035f076f910b3
Francesco Romani <fromani@redhat.com>
2020-12-02 17:22:20 +0100

    cluster: fix cluster name
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cluster/kind-replica1.sh
+++ cluster/kind-replica1.sh
@@ -35,7 +35,7 @@ EOF
 		--config ${CONTEXT}/kindconfig.yaml \
 		--image ${OPENSHIFTKNI_CI_KIND_IMAGE} \
 		--name ${CLUSTER_NAME} || exit 2
-	kubectl label node kind-worker node-role.kubernetes.io/worker=''
+	kubectl label node ${CLUSTER_NAME}-worker node-role.kubernetes.io/worker=''
 	echo "${CONTEXT}/kubeconfig"
 }
 
