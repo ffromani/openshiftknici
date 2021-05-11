b86c3c5c91b1eeedea6ae0158a9415fc25e8b966
Francesco Romani <fromani@redhat.com>
2021-05-11 12:17:32 +0200

    cluster: enable podresource FG
    The new GetAllocatableResources API is alpha, so it
    needs to be explicitly enabled using a FG.
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cluster/kind-replica1.sh
+++ cluster/kind-replica1.sh
@@ -26,6 +26,8 @@ kubeadmConfigPatches:
   cpuManagerReconcilePeriod: "5s"
   topologyManagerPolicy: "single-numa-node"
   reservedSystemCPUs: "1"
+  featureGates:
+    KubeletPodResourcesGetAllocatable: true
 nodes:
 - role: control-plane
 - role: worker
